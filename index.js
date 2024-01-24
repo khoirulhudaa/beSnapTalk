586660da0fba1821a674c68e4b6258c4b85b850b
valclassdevelop <developervalclass.com>
2024-01-24 15:48:08 +0700

    snapTlak

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const io = new Server(httpServer, {
         origin: "https://snaptalkk.vercel.app",
         methods: ["GET", "POST"]
     },
-    path: '/socket.io/(.*)'
+    path: '/socket.io', // Make sure this matches with the client
 });
 
 // Inisialisasi Socket.IO di luar fungsi penanganan HTTP
