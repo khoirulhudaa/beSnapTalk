8f020ec753e808136d8be69858959f8c6181eafd
valclassdevelop <developervalclass.com>
2024-01-24 07:22:28 +0700

    snapTalk

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const cors = require('cors')
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const dotenv = require('dotenv')
-const http = require('http')
-const chatController = require('./controllers/chatController')
-const { Server } = require('socket.io')
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const { Server } = require('socket.io');
+const chatController = require('./controllers/chatController');
+require('dotenv').config()
 
-dotenv.config();
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "https://snaptalkk.vercel.app",
+        methods: ["GET", "POST"]
+    }
+});
 
-app.use(cors())
+const corsOptions = {
+    origin: 'https://snaptalkk.vercel.app',
+    optionsSuccessStatus: 200
+};
 
-// Connected on database ft mongodb
+app.use(cors(corsOptions));
+
+// Connected on the MongoDB database
 mongoose.connect(process.env.URL_MONGOOSE, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => {
-    console.log('Successfully connect on database')
-})
-.catch((error) => {
-    console.log(error)
-})
-
+    .then(() => {
+        console.log('Successfully connect on database');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 // Middleware untuk mengatur timeout
 app.use((req, res, next) => {
@@ -33,56 +42,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware
-const checkToken = require('./middlewares/verifyToken')
+const checkToken = require('./middlewares/verifyToken');
 
 // Routers
-const accountRouter = require('./routers/authRouter')
-const chatRouter = require('./routers/chatRouter')
-const groupRouter = require('./routers/groupRouter')
+const accountRouter = require('./routers/authRouter');
+const chatRouter = require('./routers/chatRouter');
+const groupRouter = require('./routers/groupRouter');
 
-app.use('/account', accountRouter)
-app.use('/chat', chatRouter)
-app.use('/group', checkToken, groupRouter)
+app.use('/account', accountRouter);
+app.use('/chat', chatRouter);
+app.use('/group', checkToken, groupRouter);
 
 app.get('/test', (req, res) => {
-    res.send('test success!')   
-})
-
-
-
-const httpServer = http.createServer(app);
-const io = new Server(httpServer, {
-    cors: {
-        origin: "https://snaptalkk.vercel.app",
-        methods: ["GET", "POST"]
-    }
+    res.send('test success!');
 });
 
+// Inisialisasi Socket.IO di luar fungsi penanganan HTTP
 io.on('connection', async (socket) => {
-    console.log('socket.id :', socket.id)
+    console.log('socket.id:', socket.id);
 
     socket.on('chat', async (data) => {
-        console.log('data chat:', data)
-        const result = await chatController.createChat(data)
-        console.log('result create chat:', result)
-        io.emit('chat_received', result)
-    })
-    
+        console.log('data chat:', data);
+        const result = await chatController.createChat(data);
+        console.log('result create chat:', result);
+        io.emit('chat_received', result);
+    });
 
     socket.on('chat_remove', async (data) => {
-        console.log('data chat remove:', data)
-        const result = await chatController.removeChatById(data)
-        console.log('result remove chat:', result)
-        io.emit('chat_received', result)
-    })
-    
+        console.log('data chat remove:', data);
+        const result = await chatController.removeChatById(data);
+        console.log('result remove chat:', result);
+        io.emit('chat_received', result);
+    });
+
     socket.on('disconnect', () => {
-        console.log('User disconnected from ', socket.id)
-    })
+        console.log('User disconnected from ', socket.id);
+    });
 });
 
-httpServer.listen(3600, () => {
-    console.log(`Running on port ${3600}`)
-})
-
-// Running test
+module.exports = httpServer;
